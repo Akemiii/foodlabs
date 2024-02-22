4f6b823aaf2566d7d9cb0d3fed255ba22722a126
Daakemii <daakemii@gmail.com>
2024-02-22 18:15:17 -0300

    Start project

---
+++ src/test/java/com/foodlabs/FoodlabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodlabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
