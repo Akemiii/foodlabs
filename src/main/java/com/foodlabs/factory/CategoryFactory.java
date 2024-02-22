3e6002bdaa26d4f4415c0b3582d84d13c40de45b
Daakemii <daakemii@gmail.com>
2024-02-22 19:18:15 -0300

    feat: create CategoryFactory

---
+++ src/main/java/com/foodlabs/factory/CategoryFactory.java
@@ -0,0 +1,30 @@
+package com.foodlabs.factory;
+
+import com.foodlabs.DTO.request.CreateCategoryRequest;
+import com.foodlabs.DTO.response.CategoryResponse;
+import com.foodlabs.model.Category;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class CategoryFactory {
+
+    public Category createCategoryModel(CreateCategoryRequest request) {
+        return Category.builder()
+                .categoryId(UUID.randomUUID())
+                .name(request.getName())
+                .image(request.getImage())
+                .build();
+    }
+
+    public CategoryResponse createCategoryResponse(Category category) {
+        return CategoryResponse.builder()
+                .categoryId(category.getCategoryId())
+                .name(category.getName())
+                .image(category.getImage())
+                .build();
+    }
+
+
+}
