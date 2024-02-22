2457108c8efcbec717d73252edcdd9ce0a19b629
Daakemii <daakemii@gmail.com>
2024-02-22 19:26:08 -0300

    feat: update CategoryService

--- src/main/java/com/foodlabs/service/CategoryService.java
+++ src/main/java/com/foodlabs/service/CategoryService.java
@@ -1,12 +1,20 @@
 package com.foodlabs.service;
 
+import com.foodlabs.DTO.request.CreateCategoryRequest;
+import com.foodlabs.DTO.request.UpdateCategoryRequest;
+import com.foodlabs.DTO.response.CategoryResponse;
+import com.foodlabs.factory.CategoryFactory;
 import com.foodlabs.model.Category;
 import com.foodlabs.repositories.CategoryRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -14,31 +22,51 @@ import java.util.UUID;
 public class CategoryService {
 
     public final CategoryRepository repository;
+    public final CategoryFactory factory;
 
     @SneakyThrows
-    public Category findById(UUID categoryId) {
+    private Category findById(UUID categoryId) {
         return repository.findById(categoryId)
                 .orElseThrow(ChangeSetPersister.NotFoundException::new);
 
     }
 
-    public CategoryResponse getById(UUID categoryId){
-
-        return null;
+    public CategoryResponse getById(final UUID categoryId) {
+        return factory.createCategoryResponse(findById(categoryId));
     }
 
-    public CategoryRespose createCategory(final CreateCategoryRequest request){
+    public List<CategoryResponse> getAll() {
+        final var categories = repository.findAll();
 
-        return null;
+        return categories.stream().map(factory::createCategoryResponse).toList();
     }
 
-    public CategoryResponse updateCategory(final UpdateCategoryRequest request){
+    public CategoryResponse createCategory(@RequestBody @Valid final CreateCategoryRequest request) {
+        final var category = factory.createCategoryModel(request);
 
-        return null;
+        repository.save(category);
+
+        return factory.createCategoryResponse(category);
     }
 
-    public void deleteCategory(UUID categoryId){
+    public CategoryResponse updateCategory(final UUID categoryId, @RequestBody @Valid final UpdateCategoryRequest request) {
 
+        final var category = findById(categoryId);
+
+        if (Objects.nonNull(request.getName()) && !request.getName().isEmpty())
+            category.setName(request.getName());
+        if (Objects.nonNull(request.getImage()) && !request.getImage().isEmpty())
+            category.setImage(request.getImage());
+
+        repository.save(category);
+
+        return factory.createCategoryResponse(category);
+    }
+
+    public void deleteCategory(final UUID categoryId) {
+        final var category = findById(categoryId);
+
+        repository.delete(category);
     }
 
 }
