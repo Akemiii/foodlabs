2471e0c964ea91f7b100e8c1dde444e9c9128051
Daakemii <daakemii@gmail.com>
2024-02-22 19:18:26 -0300

    feat: create CategoryResponse

---
+++ src/main/java/com/foodlabs/DTO/response/CategoryResponse.java
@@ -0,0 +1,18 @@
+package com.foodlabs.DTO.response;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryResponse {
+
+    public UUID categoryId;
+    public String name;
+    public String image;
+
+}
