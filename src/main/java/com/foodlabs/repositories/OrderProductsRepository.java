1d24ae72f1fe72d21f930bbd8767d4b12e9ccfa0
Daakemii <daakemii@gmail.com>
2024-02-27 13:43:13 -0300

    fix: Remove repository unused

--- src/main/java/com/foodlabs/repositories/OrderProductsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.foodlabs.repositories;
-
-import com.foodlabs.model.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderProductsRepository extends JpaRepository<OrderItem, Long> {
-}
